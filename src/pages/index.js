8a10cd8186325e8055600b686d1f0d1d6b5478d0
Temitope Omotunde <topeomot@gmail.com>
2021-08-06 21:21:01 +0100

    add Home Bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,22 @@ import "./mystyles.scss"
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage
+      <div className="container" style={{marginTop:"10px"}}>
+      <div className="columns">
+        <div className="column is-4">
+        <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="../images/internet.png"
+        src="../images/topeomot.jpg"
       />
+        </div>
+        <div className="column is-8 title is-4">
+          Hi, I'm Tope. I am a Software Developer and this is my personal 
+          blog to share things I have learnt and applied in my journey as a Human and Programmer.
+          <hr/>
+        </div>
+      </div>
+      </div>
+      
     </Layout>
   )
 }
