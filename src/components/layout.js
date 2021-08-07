6412a0780a05ac1edf852e475301ca511b494d72
Temitope Omotunde <topeomot@gmail.com>
2021-08-07 14:13:07 +0100

    format mdx

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import { Link, useStaticQuery, graphql } from 'gatsby'
 import "../pages/mystyles.scss"
 
 const Layout = ({ pageTitle, children }) => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query {
       site {
         siteMetadata {
@@ -16,10 +16,10 @@ const Layout = ({ pageTitle, children }) => {
   return (
     <div>
       <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-      <nav className="navbar is-dark" style={{borderBottom: "1px solid #9aa0a6", justifyContent:"center"}} role="navigation" aria-label="main navigation">
+      <nav className="navbar is-dark" style={{ borderBottom: "1px solid #9aa0a6", justifyContent: "center" }} role="navigation" aria-label="main navigation">
 
         <div className="navbar-brand column is-2" data-target="navbarMenu">
-          <a className="navbar-item is-size-3 is-pulled-left" style={{fontFamily: "'Exo 2', sans-serif"}} href="https://topeomot.com">
+          <a className="navbar-item is-size-3 is-pulled-left" style={{ fontFamily: "'Exo 2', sans-serif" }} href="https://topeomot.com">
             TOPEOMOT
           </a>
           <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
@@ -31,24 +31,24 @@ const Layout = ({ pageTitle, children }) => {
         <div className="navbar-menu column is-4" id="navbarMenu">
 
           <div className="navbar-end">
-          <Link className="navbar-item" to="/">
-            Home
-          </Link>
-          <Link className="navbar-item" to="/blog">
-            Blog
-          </Link>
+            <Link className="navbar-item" to="/">
+              Home
+            </Link>
+            <a className="navbar-item" to="/blog">
+              Twitter
+            </a>
           </div>
         </div>
       </nav>
- 
-        <div className="columns">
-          <div className="column is-6 is-offset-3">
+
+      <div className="columns">
+        <div className="column  is-6-widescreen is-offset-3-widescreen is-4-until-widescreen is-offset-4-until-widescreen">
           <main>
             {children}
-            </main>
-            </div>
+          </main>
         </div>
-      
+      </div>
+
     </div>
   )
 }
